bf0db94247808c080ae0c045f3827e0ee36d9344
NaiveenAlex <108775704+NaiveenAlex@users.noreply.github.com>
2022-08-30 13:39:14 -0400

    Update minCostClimbingStairs.cpp

--- DP/minCostClimbingStairs.cpp
+++ DP/minCostClimbingStairs.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) 
     {
-        int total = 0, memo1 = 0, memo2 = 0;
+        int total = 0, cost1 = 0, cost2 = 0;
         
         if(cost.size() > 1)
         {
             for(int i = 2; i <= cost.size(); ++i)
             {
-                total = min(cost[i-1] + memo1, cost[i-2] + memo2);
-                memo2 = memo1;
-                memo1 = total;
+                total = min(cost[i-1] + cost1, cost[i-2] + cost2);
+                cost2 = cost1;
+                cost1 = total;
             }
         }
         return total;
